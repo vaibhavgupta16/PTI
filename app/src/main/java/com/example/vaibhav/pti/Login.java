860d30a289d9222da42c4dfd1844dd32b43e1e86
vaibhavgupta16 <vaibhavgupta1609@gmail.com>
2018-03-22 20:41:49 +0530

    Validations Completed

--- app/src/main/java/com/example/vaibhav/pti/Login.java
+++ app/src/main/java/com/example/vaibhav/pti/Login.java
@@ -1,19 +1,33 @@
 package com.example.vaibhav.pti;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AutoCompleteTextView;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
     Button button_login,button_newUser;
-    EditText editText_password;
-    AutoCompleteTextView autoCompleteTextView_email;
+    EditText editText_password,editText_phone;
     TextView textView_forgot;
+    RequestQueue queue;
+    String TAG="courserequest",tdphone,tdpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +38,7 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         button_newUser=findViewById(R.id.new_User);
 
         editText_password=findViewById(R.id.password);
-        autoCompleteTextView_email=findViewById(R.id.email);
+        editText_phone=findViewById(R.id.phone);
         textView_forgot=findViewById(R.id.forgot_password);
 
         button_login.setOnClickListener(this);
@@ -35,14 +49,81 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
+        Initialize();
+        final String result="Login Failed!";
         switch (v.getId()){
             case R.id.login:
-                break;
+                if(!validations()){
+
+                }
+                else {
+                    queue = Volley.newRequestQueue(this);
+                    String url = "http://iamguptag.000webhostapp.com/Register.php?act=Login&td_phone=" + tdphone + "&td_password=" + tdpassword + "";
+                    final ProgressDialog pDialog = new ProgressDialog(this);
+                    pDialog.setMessage("Loading...");
+                    pDialog.show();
+
+                    Log.e("==url==", "" + url);
+
+                    // Request a string response from the provided URL.
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Log.e("===response==", "==" + response);
+                                    pDialog.dismiss();
+
+                                    try {
+                                        JSONObject jobj = new JSONObject(response);
+                                        String success = jobj.getString("result");
+                                        if (success.equals(result)) {
+                                            Toast.makeText(getApplicationContext(), "Phone No. or Password Is Incorrect", Toast.LENGTH_SHORT).show();
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Sign in Complete", Toast.LENGTH_SHORT).show();
+                                        }
+
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("===error==", "==" + error);
+                            pDialog.dismiss();
+                        }
+                    });
+                    stringRequest.setTag(TAG);
+
+                    // Add the request to the RequestQueue.
+                    queue.add(stringRequest);
+                    break;
+                }
             case R.id.new_User:
                 Intent intent=new Intent(Login.this,Registration.class);
                 startActivity(intent);
                 break;
         }
 
+    }
+
+    public void Initialize(){
+        tdphone=editText_phone.getText().toString();
+        tdpassword=editText_password.getText().toString();
+    }
+
+    private boolean validations(){
+        boolean value=true;
+        if(tdphone.isEmpty()||tdphone.length()>11){
+            editText_phone.setError("Check The Number again");
+            value=false;
+        }
+
+        if(tdpassword.isEmpty()){
+            editText_password.setError("Please Enter The Password");
+            value=false;
+        }
+        return value;
     }
 }
--- app/src/main/java/com/example/vaibhav/pti/Registration.java
+++ app/src/main/java/com/example/vaibhav/pti/Registration.java
@@ -1,9 +1,11 @@
 package com.example.vaibhav.pti;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,63 +49,99 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        Initialize();
+        if(!validations()){
+
+        }
+        else {
+            queue = Volley.newRequestQueue(this);
+            String url = "http://iamguptag.000webhostapp.com/Register.php?act=Insertion&td_first=" + tdname + "&td_last=" + tdlast + "&td_email=" + tdemail + "&td_password=" + tdpassw + "&td_confirmpass=" + tdcpassw + "&td_phone=" + tdphone + "";
+            final ProgressDialog pDialog = new ProgressDialog(this);
+            pDialog.setMessage("Loading...");
+            pDialog.show();
+
+            // Request a string response from the provided URL.
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            Log.e("===response==", "==" + response);
+                            pDialog.dismiss();
+
+                            try {
+                                JSONObject jobj = new JSONObject(response);
+                                String success = jobj.getString("Result");
+                                Log.e("==Result==", "" + success);
+                                final String confirm="Success!";
+                                if (success.equals(confirm)) {
+                                    Toast.makeText(getApplicationContext(), "User account created successfully", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Registration.this, Login.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Some Error Occured", Toast.LENGTH_SHORT).show();
+                                }
+
+                                Log.e("==Result==", "" + success);
+
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("===error==", "==" + error);
+                    pDialog.dismiss();
+                }
+            });
+            stringRequest.setTag(TAG);
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest);
+        }
+
+    }
+
+    //GETTING THE VALUES FROM EDIT TEXT
+    public void Initialize(){
         tdname=first.getText().toString();
         tdlast=last.getText().toString();
         tdemail=email.getText().toString();
         tdphone=phone.getText().toString();
         tdpassw=passw.getText().toString();
         tdcpassw=cpassw.getText().toString();
-        final String confirm="Success!";
-        switch (v.getId()){
-            case  R.id.register:
-                queue = Volley.newRequestQueue(this);
-                String url ="http://iamguptag.000webhostapp.com/Register.php?act=Insertion&td_first="+tdname+"&td_last="+tdlast+"&td_email="+tdemail+"&td_password="+tdpassw+"&td_confirmpass="+tdcpassw+"&td_phone="+tdphone+"";
-                final ProgressDialog pDialog = new ProgressDialog(this);
-                pDialog.setMessage("Loading...");
-                pDialog.show();
+    }
 
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                Log.e("===response==","=="+response);
-                                pDialog.dismiss();
-
-                                try {
-                                    JSONObject jobj=new JSONObject(response);
-                                    String success=jobj.getString("Result");
-                                    Log.e("==Result==",""+success );
-                                    if(success.equals(confirm))
-                                    {
-                                        Toast.makeText(getApplicationContext(),"User account created successfully",Toast.LENGTH_SHORT).show();
-                                    }
-                                    else
-                                    {
-                                        Toast.makeText(getApplicationContext(),"Some Error Occured",Toast.LENGTH_SHORT).show();
-                                    }
-
-                                    Log.e("==Result==",""+success );
-
-
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("===error==","=="+error);
-                        pDialog.dismiss();
-                    }
-                });
-                stringRequest.setTag(TAG);
-
-                // Add the request to the RequestQueue.
-                queue.add(stringRequest);
-
-
-                break;
+    // VALIDATIONS FOR EACH FIELD
+    private boolean validations(){
+        boolean value=true;
+        if(tdname.isEmpty()||tdname.length()>32){
+            first.setError("Name Can't Be Empty");
+            value=false;
         }
+
+        if(tdlast.isEmpty()||tdlast.length()>32){
+            last.setError("Name Can't Be Empty");
+            value=false;
+        }
+        if(tdphone.isEmpty()||tdphone.length()>11){
+            phone.setError("Enter Valid Number");
+            value=false;
+        }
+
+        if(tdpassw.isEmpty()||tdcpassw.isEmpty()||tdpassw.length()<7){
+            passw.setError("Enter Password");
+            cpassw.setError("Enter Password");
+            value=false;
+        }
+        if (tdemail.isEmpty()|| Patterns.EMAIL_ADDRESS.matcher(tdemail).matches()) {
+            email.setError("Enter Valid Email");
+            value=false;
+        }
+        if(!tdpassw.equals(tdcpassw)){
+            cpassw.setError("Password Must Be Same");
+            value=false;
+        }
+        return value;
     }
 }
